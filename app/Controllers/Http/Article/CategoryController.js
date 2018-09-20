337eade73d75fcbe2b667b14fce16a9e3c154bf3
Oli <oli@qq.com>
2018-09-20 17:48:26 +0800

    文章栏目开发

--- app/Controllers/Http/Article/CategoryController.js
+++ app/Controllers/Http/Article/CategoryController.js
@@ -1,12 +1,15 @@
 'use strict'
 
 const ArticleCategory = use('App/Models/Article/Category')
+const Handle = use('App/Helpers/Handle')
 
 class CategoryController {
 
   async List({view}) {
     const categoryItem = await ArticleCategory.query().fetch()
-    return view.render('Article.category_list', {categoryItem: categoryItem.toJSON()})
+    const formatData = await Handle.treeSoleSort(categoryItem.toJSON())
+    console.log(formatData)
+    return view.render('Article.category_list', {categoryItem: formatData})
   }
 
 }
