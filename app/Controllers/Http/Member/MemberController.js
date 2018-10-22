36a201e38ca01c55c67f4ae5c4c8d9c6f4cb5388
huiliu <hui@qq.com>
2018-10-22 22:57:38 +0800

    开发会员模块

--- app/Controllers/Http/Member/MemberController.js
+++ app/Controllers/Http/Member/MemberController.js
@@ -1,5 +1,7 @@
 'use strict'
 
+const Member = use('App/Models/Member/Member')
+
 class MemberController {
 
   async list({view}){
@@ -8,6 +10,13 @@ class MemberController {
 
   async add({view}){
     return view.render('member.add')
+  }
+
+  async addSave({request, response}){
+    const {username, email, password, mobile} = request.all()
+
+    const user = await Member.create({username, email, password, mobile})
+    console.log(user)
   }
 
 }
--- database/migrations/1540198193926_member_schema.js
+++ database/migrations/1540198193926_member_schema.js
@@ -6,9 +6,9 @@ class MemberSchema extends Schema {
   up () {
     this.create('ni_member', (table) => {
       table.increments('ni_id')
-      table.string('username').unique().comment('会员名称')
-      table.string('password').comment('密码')
-      table.string('email').unique().comment('邮箱')
+      table.string('username').notNullable().unique().comment('会员名称')
+      table.string('password').notNullable().comment('密码')
+      table.string('email').notNullable().unique().comment('邮箱')
       table.bigInteger('mobile').unique().comment('手机')
       table.timestamps()
     })
